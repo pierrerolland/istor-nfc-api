74d74fccd999239a4efa7ff07570c1682ae3805c
Pierre Rolland <pierre@rolland.dev>
2024-03-26 18:01:50 +0100

    Cleaned imports

--- src/routes.rs
+++ src/routes.rs
@@ -1,20 +1,21 @@
-use warp::Filter;
+use warp::{Filter, Rejection};
 use warp::http::{HeaderMap, HeaderValue};
 use crate::handlers::AssignStoryBody;
 use super::handlers;
 
-
-pub fn routes() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+pub fn routes() -> impl Filter<Extract = impl warp::Reply, Error = Rejection> + Clone {
     let mut headers = HeaderMap::new();
     headers.insert("Content-type", HeaderValue::from_static("application/json;charset=UTF-8"));
 
-    get_unassigned_stories().or(assign_story()).with(warp::reply::with::headers(headers))
+    get_unassigned_stories()
+        .or(assign_story())
+        .with(warp::reply::with::headers(headers))
 }
 
-fn get_unassigned_stories() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+fn get_unassigned_stories() -> impl Filter<Extract = impl warp::Reply, Error = Rejection> + Clone {
     warp::path!("unassigned-stories").and(warp::get()).and_then(handlers::get_unassigned_stories)
 }
 
-fn assign_story() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+fn assign_story() -> impl Filter<Extract = impl warp::Reply, Error = Rejection> + Clone {
     warp::path!("assign-story").and(warp::post()).and(warp::body::json()).and_then(|body: AssignStoryBody| handlers::assign_story(body))
 }
